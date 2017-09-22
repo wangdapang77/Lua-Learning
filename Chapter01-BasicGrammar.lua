613f3fca2031f4eaee90cb7aec9fc19a21486752
mynawang <18267853511@163.com>
2017-09-22 14:19:07 +0800

    基本语法

---
+++ Chapter01-BasicGrammar.lua
@@ -0,0 +1,30 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: mynawang
+-- Date: 2017/9/22 0022
+-- Time: 13:54
+-- To change this template use File | Settings | File Templates.
+-- 基本语法
+
+--Hello World
+print("Hello World")
+
+b="Hello World"
+print(b)
+
+--[[多行注释]]
+
+--[[
+变量声明：
+1、以一个字母A到Z或a到z或下划线_开头后加上0个或多个字母，下划线，数字（0到9）
+2、最好不要使用下划线加大写字母的标示符，因为Lua的保留字也是这样的
+3、Lua不允许使用特殊字符如@,$,和%来定义标示符。
+4、Lua区分大小写
+]]
+
+--[[
+保留关键字：
+and、break、do、else、elseif、end、false、for、
+function、if、in、local、nil、not、or、repeat、
+return、then、true、until、while
+]]
